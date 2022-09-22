ccff6233a2f0dcaf58e92a335d1103b647431143
Robert Krency <bobkrency@gmail.com>
2022-09-22 10:19:12 -0400

    Comments

--- src/studentData.h
+++ src/studentData.h
@@ -11,23 +11,27 @@
 #include "student.h"
 
 /**
- * @brief 
+ * @brief Loads data from the storage file
  * 
  */
 struct students student_data_load();
 
 /**
- * @brief 
+ * @brief Saves the given student data to storage file, in csv format
  * 
  */
 void student_data_save(struct students studentData);
 
 /**
- * @brief Checks if the 
+ * @brief Checks if there is a data file already present
  * 
- * @return int 
+ * @return int File Exists (1), File does not Exist (2)
  */
 int student_data_exists();
 
-
+/**
+ * @brief Generates randomized student data
+ * 
+ * @return struct students 
+ */
 struct students student_data_init();
