f31cf2305a789f7c218478ab30caa2c835b222f3
Robert Krency <bobkrency@gmail.com>
2022-10-06 15:47:43 -0400

    yes

--- src/shared-memory.c
+++ src/shared-memory.c
@@ -79,7 +79,7 @@ void cpy_student_data_to_shared_memory(struct students students)
         shared_data->students[i].totalActiveTime = students.students[i].totalActiveTime;
         shared_data->students[i].status = students.students[i].status;
 
-        printf("%s %s %d %d %d %d %d",
+        printf("%s %s %d %d %d %d %d\n",
             shared_data->students[i].name,
             shared_data->students[i].name,
             shared_data->students[i].age,
